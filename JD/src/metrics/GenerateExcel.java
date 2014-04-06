353f2d1924010b5fb11e33804d65e35cb3bcae00
Suman Sohal <sumansohal11@gmail.com>
2014-04-06 14:10:57 -0400

    genexcel

--- JD/src/metrics/GenerateExcel.java
+++ JD/src/metrics/GenerateExcel.java
@@ -1,12 +1,6 @@
 package metrics;
 
-import java.util.ArrayList;
 
 public class GenerateExcel {
 
-	List<String> labels;
-
-	public void generateExcel() throws Exception {
-		labels = new ArrayList<String>();
-	}
 }
